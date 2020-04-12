0459bce309e34ee6bc3d106a5eb59f78a973919d
Maciej Burzyński <maciej.kollaps@gmail.com>
2020-04-12 20:19:59 +0200

    Homework of 2nd module - modifying of Task class to have createdAt (LocalDate) and Author (String) field.

--- src/main/java/pl/maltoza/tasks/Task/Task.java
+++ src/main/java/pl/maltoza/tasks/Task/Task.java
@@ -1,14 +1,18 @@
-package pl.maltoza.tasks;
+package pl.maltoza.tasks.Task;
 
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+
+import java.time.LocalDate;
 
 @Getter
 @Setter
 @ToString
 public class Task {
 
+   private String author;
    private String title;
+   private LocalDate createdAt;
 
 }
