aac5aed397de5b86e8ad32c5e7b364c0b256c0e3
Maciej Burzyński <maciej.kollaps@gmail.com>
2020-04-23 22:53:02 +0200

    Inject storage config property into application properties

--- src/main/java/pl/maltoza/tasks/StorageConfig.java
+++ src/main/java/pl/maltoza/tasks/StorageConfig.java
@@ -1,19 +1,21 @@
 package pl.maltoza.tasks;
 
+import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import pl.maltoza.tasks.Boundary.FileStorageService;
 import pl.maltoza.tasks.Boundary.StorageService;
 
 import java.nio.file.Path;
 
-@Slf4j
-@Configuration
+
+@Data
+@Component
+@ConfigurationProperties(prefix = "app.tasks")
 public class StorageConfig {
-    @Bean
-    public StorageService storageService() {
-        log.info("Registering Storage service as a bean");
-        return new FileStorageService(Path.of("/Users/maciejburzynski/Desktop/testDir"));
-    }
+    private Path addFilePath;
 }
--- src/main/java/pl/maltoza/tasks/TasktreeConfig.java
+++ src/main/java/pl/maltoza/tasks/TasktreeConfig.java
@@ -1,6 +1,7 @@
 package pl.maltoza.tasks;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import pl.maltoza.tasks.Boundary.FileStorageService;
@@ -11,13 +12,23 @@ import java.nio.file.Path;
 @Slf4j
 @Configuration
 public class TasktreeConfig {
+    StorageConfig storageConfig;
+
+    @Autowired
+    public TasktreeConfig(StorageConfig storageConfig) {
+        this.storageConfig = storageConfig;
+    }
 
     @Bean
     public Clock clock() {
         log.info("Registering clock bean");
         return new SystemClock();
     }
-
+    @Bean
+    public StorageService storageService() {
+        log.info("Registering Storage service as a bean");
+        return new FileStorageService(storageConfig.getAddFilePath());
+    }
 
 }
 
